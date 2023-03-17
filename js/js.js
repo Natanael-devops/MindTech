870b5dab8ca680f478cb09cb90d6510927577962
Natanael <dev.natanael.dev@gmail.com>
2023-03-17 19:01:56 -0300

    adicionando melhorias no JavaScript d página

--- js/js.js
+++ js/js.js
@@ -1,9 +1,33 @@
-var header = document.getElementById('header');
-var barraNavegaca0 = document.getElementById('barra-navegacao');
-var conteudo = document.getElementById('conteudo');
-var aparecerSideBar = false;
 
-function mudarSideBar() {
-    aparecerSideBar = !aparecerSideBar;
-    console.log(aparecerSideBar);
-}
+    var header = document.getElementById('header');
+    var barraNavegacao = document.getElementById('barra');
+    var conteudo = document.getElementById('conteudo');
+    var aparecerSideBar = false;
+
+    
+    function mudarSideBar() {
+        aparecerSideBar = !aparecerSideBar;
+        if (aparecerSideBar) {
+            barraNavegacao.style.marginLeft = '-10vw';
+            barraNavegacao.style.animationName = 'aparecerSideBar';
+            conteudo.style.filter = 'blur(2px)';
+        } else {
+            barraNavegacao.style.marginLeft = '-100vw';
+            barraNavegacao.style.animationName = '';
+            conteudo.style.filter = "";
+        }
+    }
+
+    function fecharSideBar() {
+        if (aparecerSideBar) {
+            mudarSideBar();
+        }
+    }
+
+    window.addEventListener('resize', function(event) {
+        if (window.innerWidth > 768 && aparecerSideBar) {
+            mudarSideBar();
+        }
+    });
+
+
