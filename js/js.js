7fdffe87d8db593bd5d5a51f8303e9367be30dbf
Natanael <dev.natanael.dev@gmail.com>
2023-03-16 21:52:33 -0300

    começando estilização da página e adicionando arquivos .js

---
+++ js/js.js
@@ -0,0 +1,9 @@
+var header = document.getElementById('header');
+var barraNavegaca0 = document.getElementById('barra-navegacao');
+var conteudo = document.getElementById('conteudo');
+var aparecerSideBar = false;
+
+function mudarSideBar() {
+    aparecerSideBar = !aparecerSideBar;
+    console.log(aparecerSideBar);
+}
