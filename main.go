ab4229acefd2ff8f1289986845752a67da69fc1c
Natanael <dev.natanael.dev@gmail.com>
2023-03-18 16:57:52 -0300

    melhorando a responsividade das listas

--- main.go
+++ main.go
@@ -1,38 +1,34 @@
 package main
 
 import (
-	"embed"
-	"html/template"
-	"io/fs"
-	"net/http"
-	"path/filepath"
-
 	"github.com/Natanael-devops/MindTech/controllers"
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed templates/*
-var tmplEmbed embed.FS
+// //go:embed templates/*
+//var tmplEmbed embed.FS
 
-//go:embed assets/*
-var staticEmbedFS embed.FS
+// //go:embed assets/*
+//var staticEmbedFS embed.FS
 
-type staticFS struct {
-	fs fs.FS
-}
+//type staticFS struct {
+//	fs fs.FS
+//}
 
-func (sfs *staticFS) Open(name string) (fs.File, error) {
-	return sfs.fs.Open(filepath.Join("assets", name))
-}
+//func (sfs *staticFS) Open(name string) (fs.File, error) {
+//	return sfs.fs.Open(filepath.Join("assets", name))
+//}
 
-var staticEmbed = &staticFS{staticEmbedFS}
+//var staticEmbed = &staticFS{staticEmbedFS}
 
 func CarregarRotas() {
 	r := gin.Default()
-	templ := template.Must(template.New("").ParseFS(tmplEmbed, "templates/*.html"))
-	r.SetHTMLTemplate(templ)
+	r.Static("/assets", "./assets")
+	r.LoadHTMLGlob("templates/*")
+	//templ := template.Must(template.New("").ParseFS(tmplEmbed, "templates/*.html"))
+	//r.SetHTMLTemplate(templ)
 
-	r.StaticFS("assets/", http.FS(staticEmbed))
+	//r.StaticFS("assets/", http.FS(staticEmbed))
 
 	r.GET("/", controllers.PaginaIndex)
 	r.GET("/contato", controllers.PaginaContato)
